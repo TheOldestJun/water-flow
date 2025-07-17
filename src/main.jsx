9b56de1de1ee47d306741b752a67b63d50006a43
Stanislav <s.v.pukha@gmail.com>
2025-07-17 13:24:32 +0300

    tailwind with heroui installed

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import { HeroUIProvider, ToastProvider } from '@heroui/react';
 import App from "./App";
+
+import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <HeroUIProvider>
+      <ToastProvider
+        placement="top-center"
+        toastOffset={60}
+        toastProps={{
+          radius: 'md',
+          color: 'primary',
+          variant: 'flat',
+          timeout: 2000,
+          hideIcon: true,
+          classNames: {
+            closeButton: 'opacity-100 absolute right-4 top-1/2 -translate-y-1/2',
+          },
+        }}
+      />
+      <App />
+    </HeroUIProvider>
   </React.StrictMode>,
 );
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
+
+import tailwindcss from '@tailwindcss/vite'
 import react from "@vitejs/plugin-react";
 
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
@@ -27,5 +29,18 @@ export default defineConfig(async () => ({
       // 3. tell vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
     },
+      // Env variables starting with the item of `envPrefix` will be exposed in tauri's source code through `import.meta.env`.
+  envPrefix: ['VITE_', 'TAURI_ENV_*'],
+  build: {
+    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+    target:
+      process.env.TAURI_ENV_PLATFORM == 'windows'
+        ? 'chrome105'
+        : 'safari13',
+    // don't minify for debug builds
+    minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
+  },
   },
 }));
